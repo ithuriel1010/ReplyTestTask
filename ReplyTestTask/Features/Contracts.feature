be09d863ff25670cd194784efc11b1cf9a6d1857
Aleksandra Wędołowska <ithuriel1010@gmail.com>
2024-01-12 20:55:03 +0100

    Simple test setup

---
+++ ReplyTestTask/Features/Contracts.feature
@@ -0,0 +1,11 @@
+﻿Feature: Contracts
+
+A short summary of the feature
+
+@tag1
+Scenario: Create contact
+	Given Login
+	When Navigate to Contacts
+	And Create new contact
+	And Open created contact
+	Then Contract data matches
---
+++ ReplyTestTask/StepDefinitions/ContractStepDefinition.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ReplyTestTask.StepDefinitions
+{
+    [Binding]
+    public sealed class Test1
+    {
+        private IWebDriver driver;
+
+        [Given(@"Login")]
+        public void GivenLogin()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://demo.1crmcloud.com";
+        }
+
+        [When(@"Navigate to Contacts")]
+        public void WhenNavigateToContacts()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Create new contact")]
+        public void WhenCreateNewContact()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Open created contact")]
+        public void WhenOpenCreatedContact()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"Contract data matches")]
+        public void ThenContractDataMatches()
+        {
+            throw new PendingStepException();
+        }
+
+    }
+}
